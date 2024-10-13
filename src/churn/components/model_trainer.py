8ac0982f17a4a7b2f5bae4c5c35fd30b98af7596
Amantyagidatascientist <tyagiaman3558@gmail.com>
2024-10-13 23:47:34 +0530

    add the data on the train file

--- src/churn/components/model_trainer.py
+++ src/churn/components/model_trainer.py
@@ -3,6 +3,7 @@ import sys
 from dataclasses import dataclass
 
 from catboost import CatBoostClassifier
+from numpy import square
 from sklearn.ensemble import AdaBoostClassifier,GradientBoostingClassifier,RandomForestClassifier
 from sklearn.linear_model import LinearRegression
 from sklearn.tree import DecisionTreeClassifier
@@ -10,6 +11,7 @@ from xgboost import XGBClassifier
 
 from src.churn.exception import CustomException
 from src.churn.logger import logging
+from src.churn.utils import read_sql_data,evaluate_models
 
 @dataclass
 
@@ -22,7 +24,62 @@ class ModelTrainer:
     
     def initiale_model_trainer(self,train,test,target,target_test):
         try:
-            pass
+            models={
+                "Random Forest" : RandomForestClassifier(),
+                "Decision Tree" : DecisionTreeClassifier(),
+                "Gradient Boosting" : GradientBoostingClassifier(),
+                "XG Boosting" : XGBClassifier(),
+                "catBoosting" : CatBoostClassifier(),
+                "AdaBoosting" : AdaBoostClassifier(),
+
+            }
+            params={
+                "Decision Tree": {
+                    "critersion":["squared_error","friedman_mse","absolute_error","poisson"],
+                    "splitter":["best","random"],
+                    "max_features":['sqrt','log2']
+                },
+                "Random Forest" :{
+                    "critersion":["squared_error","friedman_mse","absolute_error","poisson"],
+                    "n_estimators":[8,16,32,64,128,256],
+                    "max_features":['sqrt','log2']
+
+                },
+                "Gradient Boosting":{
+                    "loss":["squared_error","huber","absolute_error","quantile"],
+                    "learning_rate":[0.1,0.01,0.05,.001],
+                    "subsample": [0.6,0.7,0.75,0.8,0.85,0.9],
+                    "criterion":["squared_error","friedman_mse"],
+                    "max_features":['auto','sqrt','log2'],
+                    "n_estimators":[8,16,32,64,128,256]
+                },
+                
+                "XGBRegressor":{
+                    "learning_rate":[0.1,0.01,0.05,0.001],
+                    "n_estimators":[8,16,32,64,128,256],
+
+                },
+                "CatBoosting":{
+                    "depth":[6,8,10],
+                    "learing_rate":[0.01,0.05,0.001],
+                    "iterations":[30,50,100]
+                },
+                "AdaBoost":{
+                    "Learning_rate":[0.1,0.01,0.5,0.001],
+                    "loss":['linear',"square","exponential"],
+                    "n_estimators":[8,16,32,64,128,256]
+                }
+
+
+                }
+            
+            model_report:dict=evaluate_models(train,test,target,target_test,models,params)
+
+
+
+            
+
+
         except Exception as e:
             raise CustomException(e,sys)
      
--- src/churn/utils.py
+++ src/churn/utils.py
@@ -1,6 +1,8 @@
 import os
 import pickle
 import sys
+
+from sklearn.model_selection import GridSearchCV
 from src.churn.logger import logging
 import pandas as pd
 from src.churn.exception import CustomException
@@ -39,6 +41,39 @@ def read_sql_data():
     
     except Exception as e:
            raise CustomException(e,sys)
+    
+def evaluate_models(x_train,y_train,x_test,y_test,models,param):
+    try:
+        report={}
+
+        for i in range(len(list(models))):
+             model=list(models.values())[i]
+             para=param[list(models.keys())[i]]
+
+             gs=GridSearchCV(model,param,cv=3)
+             gs.fit(x_train,y_train)
+
+             model.set_params(**gs.best_params_)
+             model.fit(x_train,y_train)
+
+             y_train_pred=model.predict(x_train)
+             y_test_pred=model.predict(x_test)
+
+             train_model_score=
+
+             report[list(models.keys())[i]]=train_model_score
+
+             return report
+
+
+
+            
+
+
+
+    except Exception as e:
+            raise CustomException(e,sys)
+     
     
 def save_object(file_path,obj):
     try:
